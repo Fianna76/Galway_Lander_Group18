f64cb083dcd92ef73745046331c221fc68c9911c
Oisín <oisinmcdk@outlook.ie>
2024-03-12 22:27:23 +0000

    Added smoothing to accelerometer values

--- Accelometer_Initial/Accelometer_Initial.ino
+++ Accelometer_Initial/Accelometer_Initial.ino
@@ -7,6 +7,13 @@
 ================================
 ***************************************************************************/
 
+//===========================================================================
+// Import necessary libraries
+//===========================================================================
+
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <SPI.h>
 
 //===========================================================================
 // Constants - These never change
@@ -15,11 +22,6 @@
 // N.B. Every I2C device has a unique address/chip ID
 #define I2CADDR (0x76)
 #define CHIPID (0x60) //0x60 for BME and 0x58 for BMP.
-
-// Constants for use in code
-#define SEALEVELPRESSURE_HPA (1013.25) // Pressure at sea level in hPa
-#define ROCKET_MASS (0.2) // Mass of rocket/capsule in kilograms
-
 
 // For use when converting raw accelerometer values to m/s^2
 #define ONE_G_DIFF (140) // ADC value difference between 0g and 1g
@@ -36,9 +38,24 @@
 // Variables to hold acceleration readings in m/s^2
 float accel_x, accel_y, accel_z;
 
+// Moving average filter variables
+int INDEX = 0;
+int VALUE[3] = {0};
+int SUM[3] = {0};
+int X_READINGS[WINDOW_SIZE]={0};
+int Y_READINGS[WINDOW_SIZE]={0};
+int Z_READINGS[WINDOW_SIZE]={0};
+int TEMP_READINGS[WINDOW_SIZE];
+int PRESSURE_READINGS[WINDOW_SIZE];
+int X_AVERAGED = 0;
+int Y_AVERAGED = 0;
+int Z_AVERAGED = 0;
+int TEMP_AVERAGED = 0;
+int PRESSURE_AVERAGED = 0;
+
 void setup() {
   // Code has started, start timestamp.
-  time_start = millis();
+  //time_start = millis();
   
   // Setup serial connections to the HC-12/ PC
   // Start serial streams between Arduino and HC-12/ PC
@@ -59,15 +76,11 @@ void readFromAccelerometer(){
   int raw_z = analogRead(A3);
 
   // Convert from raw accelerometer values to m/s^2 using pre-calibrated values
-  // HINT: Look at the slides, use the map() function
-
-
-
+  //TODO: Properly calibrate - current values only from z testing 
   float new_x=map(raw_x,302,442,-g,g);
-  float new_y=map(raw_x,302,442,-g,g);
+  float new_y=map(raw_y,302,442,-g,g);
   float new_z=map(raw_z,302,442,-g,g);
 
-  
   
   // Store value into global variables: accel_x, accel_y, accel_z.
   accel_x = new_x;
@@ -76,15 +89,73 @@ void readFromAccelerometer(){
 
 }
 
+int average_array(int arrayToSum[], int sizeOfArray){
+  int sum, average = 0;
+  for ( int i = 0; i < sizeOfArray; i++ ){
+    sum += arrayToSum[ i ];
+  }
+
+  // divide by array size to get average 
+  average = sum / sizeOfArray;
+  
+  return average;
+}
 
 void smoothAccelReading(){
-  // Useful Example: https://maker.pro/arduino/tutorial/how-to-clean-up-noisy-sensor-data-with-a-moving-average-filter
-  // Steps:
-  // 1. index tells us which value we are replacing 
-  // 2. Read in next Values
-  // 3. calculate sum of array
-  // 4. Store the averaged/smoothed sensor value into global variable
+  //TODO: Comment properly, and see if you can refactor this to work without having to change the global variables from the example code 
+  //TODO: They GAVE us an average_array function - USE IT
+  //I'm 99% sure they did not intend for me to use arrays like this here
+  //This is a crime against nature and god will judge me for my sins
+  
+  int temp[3];
 
+  //Removes the oldest entries from the sum
+  temp[0]=SUM[0]-X_READINGS[INDEX];
+  temp[1]=SUM[1]-Y_READINGS[INDEX];
+  temp[2]=SUM[2]-Z_READINGS[INDEX];
+  
+  //Store all the axis readings as items in an array
+  VALUE[0] = accel_x;
+  VALUE[1] = accel_y;
+  VALUE[2] = accel_z;
+
+  //Add each reading to the window 
+  X_READINGS[INDEX] = VALUE[0];  
+  Y_READINGS[INDEX] = VALUE[1];
+  Z_READINGS[INDEX] = VALUE[2];
+
+  //Add the newest readings to the SUM
+  for(int i=0;i<3;i++) {
+    SUM[i] = temp[i] + VALUE[i];
+    /*DEBUG CODE: Archived
+    Serial.println(i);
+    Serial.print("=Sum=");
+    Serial.println(SUM[i]);
+    Serial.print("=Value=");
+    Serial.println(VALUE[i]);
+    Serial.print("=Temp=");
+    Serial.println(temp[i]);
+    Serial.print("=Index=");
+    Serial.println(INDEX);
+    delay(2000);
+    */
+  }
+    /* DEBUG CODE: Archived
+    Serial.print("=X=");
+    Serial.println(X_READINGS[INDEX]);
+    Serial.print("=Y=");
+    Serial.println(Y_READINGS[INDEX]);
+    Serial.print("=Z=");
+    Serial.println(Z_READINGS[INDEX]);
+    */
+    
+
+  //Stores the average                  
+  X_AVERAGED = SUM[0] / WINDOW_SIZE;
+  Y_AVERAGED = SUM[1] / WINDOW_SIZE;
+  Z_AVERAGED = SUM[2] / WINDOW_SIZE;
+
+  
 }
 
 float calcVelocity(char direction_char, float acceleration, unsigned long diff_time){
@@ -108,8 +179,19 @@ void loop() {
   readFromAccelerometer();
   smoothAccelReading();
 
+  INDEX = (INDEX+1) % WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
+
+  // GENERATE TRANSMISSION STRING
+  String data_to_send = "";
+  // Use commas to separate values to generate transmssion string
+  data_to_send = String(X_AVERAGED) + "," + String(Y_AVERAGED) + "," + String(Z_AVERAGED) + ",";
+  //data_to_send = String(accel_x) + "," + String(accel_y) + "," + String(accel_z) + ",";
+
+
+  Serial.println(data_to_send);
+  delay(100);
   //Serial.println(accel_x);
-  Serial.println(accel_y);
+  //Serial.println(accel_y);
   //Serial.println(accel_z);
 
 }
