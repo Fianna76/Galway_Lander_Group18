effbd4928c925ff2a8447b8ba110cf8f4264a346
Oisín <oisinmcdk@outlook.ie>
2024-03-26 16:15:23 +0000

    Finished

--- Final/Final.ino
+++ Final/Final.ino
@@ -289,6 +289,8 @@ float calcVelocity(char direction_char, float acceleration, unsigned long diff_t
   int intcheck=0;
   bool check=false; 
 
+  float adjusted_accel = acceleration-9;
+
   //Convert diff_time to seconds 
   float current_time = static_cast<float>(diff_time) / 1000;
   //This static cast is neccesary, as I was getting persistent issues with the conversion without it
@@ -300,8 +302,10 @@ float calcVelocity(char direction_char, float acceleration, unsigned long diff_t
     prev_x=velocity-prev_x;
     break; 
 
+    /*For y - since the sensor is placed vertically "up" along the y axis, we must account for gravity which would otherwise persistently generate a velocity#
+    even while the sensor remained at rest. The other axis are fine without this as they shouldn't be affected by gravity unless the rocket goes sideways */
     case 'y':
-    velocity = prev_y + acceleration*current_time;
+    velocity = prev_y + adjusted_accel*current_time;
     prev_y=velocity-prev_y;
     break;
 
@@ -333,39 +337,38 @@ float calcForce(float acceleration){
   return force;
 }
 
-int detectMode(int prev_mode){
-  int mode_code; 
+int detectMode(int prev_mode){ 
   //Note - mode changes shouldn't really be a big thing for the live transmission within the final test - but we do have to track the times of each mode change
 
   if (prev_mode == 0) {
-    mode = PRE_LAUNCH;
-    prev_mode = PRE_LAUNCH;
+    mode = 0;
+    prev_mode = 0;
 
     mode_time[0]=time_prev;
   }
 
   //TODO: Set this to something feasible based off altitude at ground
-  else if(prev_mode == PRE_LAUNCH && curr_alt > GROUND_LEVEL) {
-    mode = ASCENDING;
-    prev_mode = ASCENDING;
+  else if(prev_mode == 0 && curr_alt > GROUND_LEVEL) {
+    mode = 1;
+    prev_mode = 1;
 
     mode_time[1]=time_prev;
   }
 
-  else if (prev_mode == ASCENDING && curr_alt < max_alt) {
-    mode = DESCENDING;
-    prev_mode = DESCENDING;
+  else if (prev_mode == 1 && curr_alt < max_alt) {
+    mode = 2;
+    prev_mode = 2;
 
     mode_time[2]=time_prev;
   }
 
-  else if (prev_mode == DESCENDING && curr_alt == min_alt) {
-    mode = LANDED;
-    prev_mode = LANDED;
+  else if (prev_mode == 2 && curr_alt == min_alt) {
+    mode = 3;
+    prev_mode = 3;
 
     mode_time[3]=time_prev;
   }
-  return mode_code; 
+  return mode; 
 }
 
 //===========================================================================
@@ -758,16 +761,19 @@ void loop() {
 
   // GENERATE TRANSMISSION STRING
   String data_to_send = "";
+
+  data_to_send = data_to_send + vel_x + "," + vel_y + "," +vel_z;
+  transmit(data_to_send);
+
   
-  //Switch statement for different modes goes HERE
+  data_to_send = "";
    switch(mode_code){
-    case PRE_LAUNCH:
+    case 0: //PRE_LAUNCH
       // Only transmit once every 5s, use some kind of timer.
-      if (((time_prev - pre_launch_last_transmit_time) % 5) == 0){
+      if ((time_prev - pre_launch_last_transmit_time) > 5000){
         data_to_send = data_to_send + "Current Mode: PRE_LAUNCH ";
         data_to_send = data_to_send + "," + curr_alt + "," + accel_y;
         data_to_send = data_to_send + "5 seconds since last PRE_LAUNCH transmit - transmit since start: " + time_prev;
-        Serial.println(data_to_send);
        
         // Transmit data
         transmit(data_to_send);
@@ -780,7 +786,7 @@ void loop() {
       }
       break;
       
-    case ASCENDING:
+    case 1: //ASCENDING
       // Transmit as fast as possible
       // Package data into a string
         data_to_send = data_to_send + X_AVERAGED + "," + Y_AVERAGED + "," + Z_AVERAGED + "," + TEMP_AVERAGED + "," + PRESSURE_AVERAGED + "," + curr_alt;
@@ -791,7 +797,7 @@ void loop() {
       transmit(data_to_send);
       break;
       
-    case DESCENDING:
+    case 2: //Descending
       // Transmit as fast as possible
       // Package data into a string
         data_to_send = data_to_send + X_AVERAGED + "," + Y_AVERAGED + "," + Z_AVERAGED + "," + TEMP_AVERAGED + "," + PRESSURE_AVERAGED + "," + curr_alt;
@@ -802,7 +808,7 @@ void loop() {
       transmit(data_to_send);
       break;
 
-    case LANDED: 
+    case 3: //LANDED 
       bool summary=false;
       if (summary==false) {
         // Provide summary 
@@ -843,5 +849,6 @@ void loop() {
 
     default:
       break;
+      
   }
 }
